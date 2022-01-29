c49de1f3646f3826ff71188ac1c2f16b8216ca39
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2022-01-29 13:05:59 -0300

    refactor

--- Player.gd
+++ Player.gd
@@ -3,24 +3,29 @@ extends KinematicBody2D
 var score:int = 0
 
 var speed:int = 200
-var jumpFoce:int = 600
-var gravity:int = 800
+var jumpFoce:int = 900
+var gravity:int = 1600
 
 var vel:Vector2 = Vector2(0,0)
 
 onready var sprite: AnimatedSprite = get_node("Sprite")
 
-func _physics_process(delta):
-	vel.x = 0
-	
-	if Input.is_action_pressed("move_left1"):
-		vel.x -= speed
-		sprite.play("run")
-	elif Input.is_action_pressed("move_right1"):
-		vel.x += speed
+func _process(delta):
+	if vel.x != 0:
 		sprite.play("run")
 	else:
 		sprite.play("idle")
+		
+
+func _physics_process(delta):
+	#vel.x = 0
+	
+	if Input.is_action_pressed("move_left1"):
+		vel.x = -speed
+	elif Input.is_action_pressed("move_right1"):
+		vel.x = speed
+	else:
+		vel.x = 0	
 	
 	
 	vel = move_and_slide(vel, Vector2.UP)
