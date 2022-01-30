66bb6133b7b63926e7aca93c0e1cd0937cc71cab
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2022-01-30 00:23:40 -0300

    Cold and Hot fires

--- playerbase.gd
+++ playerbase.gd
@@ -3,12 +3,15 @@ extends KinematicBody2D
 var score:int = 0
 
 var speed:int = 200
-var jumpFoce:int = 900
+var jumpFoce:int = 700
 var gravity:int = 1600
+var speep_wall_jump: int = 2000
 
 var move_left: String = "move_left2"
 var move_right: String = "move_right2"
 var jump: String = "jump2"
+
+var jump_count: int = 0
 
 var vel:Vector2 = Vector2(0,0)
 
@@ -30,7 +33,8 @@ func _process(delta):
 		sprite.play("idle")
 
 func _physics_process(delta):
-	#vel.x = 0
+	if is_on_floor() or is_on_wall():
+		jump_count = 0
 	
 	if Input.is_action_pressed(move_left):
 		vel.x = -speed
@@ -39,12 +43,21 @@ func _physics_process(delta):
 	else:
 		vel.x = 0	
 	
-	
 	vel = move_and_slide(vel, Vector2.UP)
 	
 	vel.y += gravity * delta
-	if Input.is_action_just_pressed(jump) and is_on_floor():
-		vel.y -= jumpFoce
+	if Input.is_action_just_pressed(jump) and (is_on_floor() or jump_count < 1):
+		vel.y = -jumpFoce
+		if is_on_wall():
+			if sprite.flip_h:
+				vel.x = speep_wall_jump
+				vel = move_and_slide(vel, Vector2.LEFT)
+			else:
+				vel.x = - speep_wall_jump
+				vel = move_and_slide(vel, Vector2.RIGHT)
+				
+			
+		jump_count += 1
 		
 	
 	if vel.x < 0:
