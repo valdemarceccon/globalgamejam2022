669ec4ee4c11857f31ef0af8941e6fc335621fad
allanbraun <allanvobraun@gmail.com>
2022-01-30 12:30:47 -0300

    mapa terminado v2

--- MainScene.gd
+++ MainScene.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _ready():
 	$Hud.show()
 	$HotPlayer.score_callback = funcref($Hud, "update_hot")
-	$ColdPlayer.score_callback = funcref($Hud, "update_cold")
+	#$ColdPlayer.score_callback = funcref($Hud, "update_cold")
